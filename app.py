233c10af787be49a9d77a43224ad6e5c35897d5d
paola <swan.madariaga@gmail.com>
2022-11-12 11:04:25 -0400

    templates

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
-from flask import Flask
-from flask import request
-from flask import render_template 
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
 
 @app.route('/')
-def hello_wordl():
-    return 'Hola Mundo'
+def index():
+    return render_template('index.html', messages = messages)
 
 @app.route('/usuario/<name>')
 def user(name):
-    return render_template("user.html", name = name)
+    return render_template("user.html", user = name)  
+
+@app.route('/usuario')
+def user_incognito():
+    return render_template('user.html')
 
 @app.route('/navegador')
 def browser():
-    user_agent = request.headers.get("User-Agent")
-    return f'Tu navegador es: {user_agent}'
+    user_agent = request.headers.get('User-Agent')
+    return f"tu naegador es: {user_agent}"
 
 @app.route('/rutas')
 def routes():
     print(app.url_map)
-    return "revisa tu consola para ver las rutas"
+    return "revisa tu consola para ver las rutas"    
 
 @app.errorhandler(404)
 def page_not_found(error):
