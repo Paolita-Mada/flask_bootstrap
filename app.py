697ecb7feb34a35b42f68c345512a2379c4803f5
paola <swan.madariaga@gmail.com>
2022-11-14 08:47:36 -0400

    templates

--- app.py
+++ app.py
@@ -1,52 +1,41 @@
 from flask import Flask, request, render_template, url_for, flash, redirect
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cualquier cosa'
-
+app.config['SECRET_KEY'] = 'Escribe cualquier cosa'
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
             {'title': 'Message Two',
              'content': 'Message Two Content'}
             ]
-
 @app.route('/')
 def index():
     return render_template('index.html', messages = messages)
-
-@app.route('/create', methods =('GET', 'POST'))
+@app.route('/create', methods = ('GET', 'POST'))
 def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-
         if not title:
-            flash('el titulo es obligatorio')
+            flash('El título es obligatorio')
         elif not content:
-            flash('el contenido es obligatorio')
+            flash('El contenido es obligatorio')
         else:
-            messages.append(['title': title, 'content': content])
+            messages.append({'title': title, 'content':content})
             return redirect(url_for('index'))
-                    
     return render_template('create.html')
-
 @app.route('/usuario/<name>')
 def user(name):
-    return render_template("user.html", user = name)  
-
+    return render_template('user.html', user = name)
 @app.route('/usuario')
 def user_incognito():
     return render_template('user.html')
-
 @app.route('/navegador')
 def browser():
     user_agent = request.headers.get('User-Agent')
-    return f"tu naegador es: {user_agent}"
-
+    return f'Tu navegador es: {user_agent}'
 @app.route('/rutas')
 def routes():
     print(app.url_map)
-    return "revisa tu consola para ver las rutas"    
-
+    return 'Revisa tu consola para ver las rutas'
 @app.errorhandler(404)
 def page_not_found(error):
-  return render_template('page_not_found.html'), 400          
+    return render_template('page_not_found.html'), 400         
