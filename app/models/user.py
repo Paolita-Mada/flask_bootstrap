081c5a026b0401b5016ee765c906ef3cbfaaf733
paola <swan.madariaga@gmail.com>
2022-12-07 11:15:08 -0400

    factorizacion 2

--- app/models/user.py
+++ app/models/user.py
@@ -7,7 +7,8 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique = True, index = True)
     username = db.Column(db.String(64), unique = True, index = True)
-    password_hash = db.Column(db.String(128), nullable = False)    
+    password_hash = db.Column(db.String(128), nullable = False)
+    messages = db.relationship('Message', backref='user')    
 
     @property
     def password(self):
